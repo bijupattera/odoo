20949875a1266325eba6aea15ca011ecf2959765
BijuPattera <bijupattera@gmail.com>
2018-11-06 14:36:13 +0300

    hi

--- oa/models/models.py
+++ oa/models/models.py
@@ -3,11 +3,12 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'oa.course'
-    _description = 'OPen Academy Course Class'
+    _description = 'Open Academy Course Class'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many('oa.session', 'course_id', string="Sessions")
 
 class Session(models.Model):
     _name = 'oa.session'
@@ -16,6 +17,7 @@ class Session(models.Model):
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(5, 2), help="Number of days")
-    seats = fields.Integer(string="Number of Seats")
+    seats = fields.Integer(string="Seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor")
-    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    course_id = fields.Many2one('oa.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
