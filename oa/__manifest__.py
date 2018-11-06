d6848f1dd371d2421d5d974cb56e09ea657e1590
BijuPattera <bijupattera@gmail.com>
2018-11-06 10:56:22 +0300

    hi

--- oa/__manifest__.py
+++ oa/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-'name': "OpenAcademy1",
+'name': "Open Academy",
 
     'summary': """
         Manage Trainings""",
--- oa/models/models.py
+++ oa/models/models.py
@@ -3,6 +3,6 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'oa.course'
-
+    _description = 'OPen Academy Course Class'
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
