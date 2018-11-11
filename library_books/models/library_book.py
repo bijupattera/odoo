3249d9150b641b45098e6216f0e3908754a35c39
BijuPattera <bijupattera@gmail.com>
2018-11-11 14:55:57 +0300

    added Library

--- library_books/models/library_book.py
+++ library_books/models/library_book.py
@@ -1,28 +1,47 @@
 # -*- coding: utf-8 -*-
-from oodoo import models, fields
+from odoo import models, fields
 class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
     _order = 'date_release desc, name'
     _rec_name = 'short_name'
     name = fields.Char('Title', required=True)
-    short_name = fields.Char('Short Title')
+    short_name = fields.Char(
+        string='Short Title',
+        size=100,  # For Char only
+        translate=False,  # also for Text fields
+    )
     notes = fields.Text('Internal Notes')
     state = fields.Selection(
         [('draft', 'Not Available'),
          ('available', 'Available'),
          ('lost', 'Lost')],
         'State')
-    description = fields.Html('Description')
+    description = fields.Html(
+        string='Description',
+        # optional:
+        sanitize=True,
+        strip_style=False,
+        translate=False,
+    )
     cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
-
-    def name_get(self):
-        result = []
-            for record in self:
-            result.append(
-            (record.id,
-                u"%s (%s)" % (record.name, record.date_released)
-    ))
-    return result
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer(
+        string='Number of Pages',
+        default=0,
+        help='Total book page count',
+        groups='base.group_user',
+        states={'cancel': [('readonly', True)]},
+        copy=True,
+        index=False,
+        readonly=False,
+        required=False,
+        company_dependent=False,
+    )
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        (14, 4),  # Optional precision (total, decimals),
+    )
