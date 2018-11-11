b677031c134eebdf1c59c4a5467a60ef97ed1d4e
BijuPattera <bijupattera@gmail.com>
2018-11-11 13:41:52 +0300

    added Library

--- library_books/models/library_book.py
+++ library_books/models/library_book.py
@@ -6,43 +6,17 @@ class LibraryBook(models.Model):
     _order = 'date_release desc, name'
     _rec_name = 'short_name'
     name = fields.Char('Title', required=True)
-    short_name = fields.Char(
-        string='Short Title',
-        size=100,  # For Char only
-        translate=False,  # also for Text fields
-    )
+    short_name = fields.Char('Short Title')
     notes = fields.Text('Internal Notes')
     state = fields.Selection(
         [('draft', 'Not Available'),
          ('available', 'Available'),
          ('lost', 'Lost')],
         'State')
-    description = fields.Html(
-        string='Description',
-        # optional:
-        sanitize=True,
-        strip_style=False,
-        translate=False,
-    )
+    description = fields.Html('Description')
     cover = fields.Binary('Book Cover')
-    out_of_print = fields.Boolean('Out of Print?')
     date_release = fields.Date('Release Date')
-    date_updated = fields.Datetime('Last Updated')
-    pages = fields.Integer('Number of Pages')
-
     author_ids = fields.Many2many('res.partner', string='Authors')
-    pages = fields.Integer(
-        string='Number of Pages',
-        default=0,
-        help='Total book page count',
-        groups='base.group_user',
-        states={'cancel': [('readonly', True)]},
-        copy=True,
-        index=False,
-        readonly=False,
-        required=False,
-        company_dependent=False,
-    )
 
     def name_get(self):
         result = []
